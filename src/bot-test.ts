8add9efc4c0f65cc6ef1e19ea780fc5fdc0d7f68
rsebastia <rsebastia@ibinn.com>
2025-05-21 11:21:16 +0200

    bot otaku cierra anuncions

---
+++ src/bot-test.ts
@@ -0,0 +1,39 @@
+import puppeteer from 'puppeteer-core';
+
+export async function runBot() {
+  const browser = await puppeteer.launch({
+    headless: false, // pon en true para que no se vea el navegador
+    executablePath: '/usr/bin/google-chrome',
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+    ],
+  });
+
+  const page = await browser.newPage();
+
+  // Detectar cuando se abre una nueva ventana/pestaña
+  browser.on('targetcreated', async (target) => {
+    if (target.type() === 'page') {
+      const newPage = await target.page();
+      if (newPage) {
+        console.log('Nueva ventana abierta, cerrándola...');
+        await newPage.close();
+      }
+    }
+  });
+
+  // poner aqui la url del video
+  await page.goto('https://www3.animeflv.net/', { waitUntil: 'networkidle2' });
+
+
+  console.log('Página principal abierta y lista.');
+
+  // Esperar 15 segundos 
+  // await new Promise(resolve => setTimeout(resolve, 15000));
+
+  // Esto es pa cerrar el navegador
+  // await browser.close();
+}
+
+runBot().catch(console.error);
